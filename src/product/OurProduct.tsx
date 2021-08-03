0588a237b9660c663f3ceb379936061bcbf57289
retno sonjaya <beno.sons@khansia.co.id>
2021-08-03 20:36:24 +0700

    update

--- src/product/OurProduct.tsx
+++ src/product/OurProduct.tsx
@@ -10,68 +10,88 @@ import React from 'react';
 //   reverse?: boolean;
 // };
 
-const OurProduct = () => {
- 
-
-  return (
-    <div className="container">
+const OurProduct = () => (
+  <div className="container">
     <div className="row">
-       <div className="col-md-12">
-             <div className="section-title text-center">
-                  <h1>varian rasa</h1>
-                  <div className="chipsofic-subtitle">Pilih sesuai dengan kesukaan</div>
-             </div>
-         </div>
+      <div className="col-md-12">
+        <div className="section-title text-center">
+          <h1>varian rasa</h1>
+          <div className="chipsofic-subtitle">Pilih sesuai dengan kesukaan</div>
+        </div>
       </div>
-      
-     <div className="choose-our-chips-content-area text-center wow fadeInUp">
-          <div className="row">
-              <div className="col-sm-6 col-md-2 single-chips-item">
-                <img src={`${process.env.baseUrl}/assets/img/chips-01.png`} alt=""/>
-                 <h3>Original</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 13.500</div>
-                  <p>Rasa natural dan gurih dengan aroma bawang yang menambah cita rasa.</p>
-                  <a className="btn chipsofic-small-btn" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Original">Pesan</a>
-              </div>
-              <div className="col-sm-6 col-md-2 single-chips-item">
-              <img src={`${process.env.baseUrl}/assets/img/chips-02.png`} alt=""/>
-                 <h3>Keju</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 13.500</div>
-                  <p>Rasa khasnya keju original sangat terasa dengan dominan rasa manis.</p>
-                  <a className="btn chipsofic-small-btn" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Keju">Pesan</a>
-              </div>
-              <div className="col-sm-6 col-md-2 single-chips-item">
-              <img src={`${process.env.baseUrl}/assets/img/chips-03.png`} alt=""/>
-                 <h3>Balado</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 13.500</div>
-                  <p>Campuran rasa pedas, manis serta gurihnya cukup kuat dan pas dilidah.</p>
-                  <a className="btn chipsofic-small-btn" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Balado">Pesan</a>
-              </div>
-              <div className="col-sm-6 col-md-2 single-chips-item">
-              <img src={`${process.env.baseUrl}/assets/img/chips-04.png`} alt=""/>
-                 <h3>Coklat</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 14.500</div>
-                  <p>Rasa dari Coklat kualitas terbaik, manisnya pas dan rasa coklat yg tebal.</p>
-                  <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Coklat">Pesan</a>
-              </div>
-              <div className="col-sm-6 col-md-2 single-chips-item">
-              <img src={`${process.env.baseUrl}/assets/img/chips-05.png`} alt=""/>
-                 <h3>Pedas Level 1</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 13.500</div>
-                  <p>Rasa pedas yang memanjakan lidah, sampai membuat ketagihan.</p>
-                  <a className="btn chipsofic-small-btn" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Level-1">Pesan</a>
-              </div>
-              <div className="col-sm-6 col-md-2 single-chips-item">
-              <img src={`${process.env.baseUrl}/assets/img/chips-06.png`} alt=""/>
-                 <h3>Pedas Level 2</h3>
-                 <div className="pricing"><span>Harga:</span> Rp. 13.500</div>
-                  <p>Bagi kalian pecinta pedas wajib mencoba dan rasakan sensasinya.</p>
-                  <a className="btn chipsofic-small-btn" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Level-2">Pesan</a>
-              </div>
+    </div>
+
+    <div className="choose-our-chips-content-area text-center wow fadeInUp">
+      <div className="row">
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-01.png`} alt="" />
+          <h3>Original</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 13.500
           </div>
-      </div>  
+          <p>Rasa natural dan gurih dengan aroma bawang yang menambah cita rasa.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Original" rel="noreferrer">Pesan</a>
+        </div>
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-02.png`} alt="" />
+          <h3>Keju</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 13.500
+          </div>
+          <p>Rasa khasnya keju original sangat terasa dengan dominan rasa manis.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Keju" rel="noreferrer">Pesan</a>
+        </div>
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-03.png`} alt="" />
+          <h3>Balado</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 13.500
+          </div>
+          <p>Campuran rasa pedas, manis serta gurihnya cukup kuat dan pas dilidah.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Balado" rel="noreferrer">Pesan</a>
+        </div>
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-04.png`} alt="" />
+          <h3>Coklat</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 14.500
+          </div>
+          <p>Rasa dari Coklat kualitas terbaik, manisnya pas dan rasa coklat yg tebal.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Coklat" rel="noreferrer">Pesan</a>
+        </div>
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-05.png`} alt="" />
+          <h3>Pedas Level 1</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 13.500
+          </div>
+          <p>Rasa pedas yang memanjakan lidah, sampai membuat ketagihan.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Level-1" rel="noreferrer">Pesan</a>
+        </div>
+        <div className="col-sm-6 col-md-2 single-chips-item">
+          <img src={`${process.env.baseUrl}/assets/img/chips-06.png`} alt="" />
+          <h3>Pedas Level 2</h3>
+          <div className="pricing">
+            <span>Harga:</span>
+            {' '}
+            Rp. 13.500
+          </div>
+          <p>Bagi kalian pecinta pedas wajib mencoba dan rasakan sensasinya.</p>
+          <a className="btn chipsofic-small-btn" target="_blank" data-scroll href="https://api.whatsapp.com/send?phone=6282218401847&text=Halo%20Keripik%20iLiL%20Order%20dong%20yang%20Level-2" rel="noreferrer">Pesan</a>
+        </div>
+      </div>
+    </div>
   </div>
-  );
-};
+);
 
 export { OurProduct };
