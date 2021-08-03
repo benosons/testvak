4bae33fef5f5006ea123b254c3857565216f27c7
retno sonjaya <beno.sons@khansia.co.id>
2021-08-03 10:32:06 +0700

    update image

--- src/gallery/OurGallery.tsx
+++ src/gallery/OurGallery.tsx
@@ -10,80 +10,52 @@ import React from 'react';
 //   reverse?: boolean;
 // };
 
-const OurGallery = () => {
- 
-
-  return (
+const OurGallery = () => (
   <div>
-  <div className="container">
-    <div className="row">
-      <div className="col-md-12">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
           <div className="section-title text-center">
             <h1>Galeri</h1>
             <div className="chipsofic-subtitle">Koleksi Foto Keripik iLiL</div>
           </div>
+        </div>
       </div>
     </div>
-    </div>
-      
-      <div className="gallery-area">
-        <div className="container-fluid">
-          <div className="row wow fadeInUp">
 
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-01.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-01.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-02.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-02.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-03.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-03.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-04.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-04.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-05.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-05.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-06.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-06.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-07.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-07.png`} alt=""/>
-                  </a>
-            </div>
-            <div className="col-sm-6 col-md-3">
-                <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-08.png`}>
-                  <i className="fa fa-plus"></i> 
-                  <img src={`${process.env.baseUrl}/assets/img/gallery-img-08.png`} alt=""/>
-                  </a>
-            </div>
-            
+    <div className="gallery-area">
+      <div className="container-fluid">
+        <div className="row wow fadeInUp">
+
+          <div className="col-sm-6 col-md-3">
+            <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-01.png`}>
+              <i className="fa fa-plus" />
+              <img src={`${process.env.baseUrl}/assets/img/gallery-img-01.png`} alt="" />
+            </a>
           </div>
+          <div className="col-sm-6 col-md-3">
+            <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-02.png`}>
+              <i className="fa fa-plus" />
+              <img src={`${process.env.baseUrl}/assets/img/gallery-img-02.png`} alt="" />
+            </a>
+          </div>
+          <div className="col-sm-6 col-md-3">
+            <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-03.png`}>
+              <i className="fa fa-plus" />
+              <img src={`${process.env.baseUrl}/assets/img/gallery-img-03.png`} alt="" />
+            </a>
+          </div>
+          <div className="col-sm-6 col-md-3">
+            <a title="Lorem Ipsum 01" className="gallery-lightbox" href={`${process.env.baseUrl}/assets/img/gallery-img-04.png`}>
+              <i className="fa fa-plus" />
+              <img src={`${process.env.baseUrl}/assets/img/gallery-img-04.png`} alt="" />
+            </a>
+          </div>
+
         </div>
-      </div>  
+      </div>
+    </div>
   </div>
-  );
-};
+);
 
 export { OurGallery };
